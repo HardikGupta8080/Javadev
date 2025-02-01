28309f0b8990dafa6f10268d63229d1c679cdce3
Hardik <hardikgupta8080@gmail.com>
2025-02-01 14:10:21 +0530

    java

---
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.demo.Service;
+
+import com.example.demo.Model.User;
+import com.example.demo.Repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepo userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import com.example.demo.Model.User;
+import com.example.demo.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+}
